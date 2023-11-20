345969b16feb9250a2411057da7b289828c42411
Mari Carmen <mdclg3@um.es>
2023-11-20 23:28:24 +0100

    Semana 11: Introducción a JSF

---
+++ ejemplosPersistencia/src/main/java/web/ConfigurationBean.java
@@ -0,0 +1,10 @@
+package web;
+
+import java.io.Serializable;
+
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+public class ConfigurationBean implements Serializable{
+
+}
